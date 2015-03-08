950b6de79f901c5cd390ee4195c24a442efb80a7
nruest <ruestn@gmail.com>
2015-03-07 20:05:58 -0500

    Add FFmpeg. Closes #8

--- Vagrantfile
+++ Vagrantfile
@@ -30,6 +30,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.provision :shell, :path => "fcrepo.sh", :args => home_dir
   config.vm.provision :shell, :path => "drupal.sh", :args => home_dir
   config.vm.provision :shell, :path => "tesseract.sh"
+  config.vm.provision :shell, :path => "ffmpeg.sh"
 
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
---
+++ ffmpeg.sh
@@ -0,0 +1,13 @@
+echo "Installing FFmpeg."
+
+FFMPEG_VERSION=1.1.4
+
+sudo sed -i '/^# deb.*multiverse/ s/^# //' /etc/apt/sources.list && sudo apt-get update && sudo apt-get install libfaac-dev -y --force-yes
+
+sudo apt-get install autoconf automake build-essential libass-dev libfreetype6-dev libgpac-dev libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libx11-dev libxext-dev libxfixes-dev pkg-config texi2html zlib1g-dev yasm libx264-dev libmp3lame-dev unzip x264 libgsm1-dev libopencore-amrnb-dev libopencore-amrwb-dev libopenjpeg-dev libschroedinger-dev libspeex-dev libvpx-dev libxvidcore-dev libdc1394-22-dev -y --force-yes
+
+cd /tmp
+wget "http://www.ffmpeg.org/releases/ffmpeg-$FFMPEG_VERSION.tar.gz"
+tar -xzvf ffmpeg-$FFMPEG_VERSION.tar.gz
+
+cd ffmpeg-$FFMPEG_VERSION && ./configure --enable-gpl --enable-version3 --enable-nonfree --enable-postproc --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libdc1394 --enable-libfaac --enable-libgsm --enable-libmp3lame --enable-libopenjpeg --enable-libschroedinger --enable-libspeex --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libxvid && make && sudo make install && sudo ldconfig
