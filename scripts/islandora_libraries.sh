96dee482321df4b653470a7d828414d62ecc7415
Logan Cox <lmc@ou.edu>
2015-06-04 15:12:53 -0500

    moved enabling of islandora_openseadragon from libraries to islandora_enable

--- scripts/islandora_libraries.sh
+++ scripts/islandora_libraries.sh
@@ -16,7 +16,6 @@ sudo drush -v pdfjs-plugin
 sudo drush -v iabookreader-plugin
 sudo drush -v colorbox-plugin
 sudo drush -v openseadragon-plugin
-sudo drush -v -y en islandora_openseadragon
 
 # After last drush call from root user, change cache permissions
 sudo chown -R vagrant:vagrant "$HOME_DIR"/.drush
