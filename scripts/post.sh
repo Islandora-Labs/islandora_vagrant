5e87d7414db20fc952ae3475b8d920269fcb1c79
Diego Pino Navarro <dpino@metro.org>
2017-03-13 16:05:08 -0400

    7.x-1.9 RC1 Vagrant Box update
    Just added minor cosmetic “feature “ other than the normal release
    branch specific things

--- scripts/post.sh
+++ scripts/post.sh
@@ -12,3 +12,31 @@ chmod -R 775 /var/www/drupal/sites/default/files
 drush --root=/var/www/drupal role-add-perm "anonymous user" "view fedora repository objects"
 drush --root=/var/www/drupal role-add-perm "authenticated user" "view fedora repository objects"
 drush --root=/var/www/drupal cc all
+
+#Last chance to fix locale
+
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
+
+# Lets brand this a bit
+
+cat <<'EOT' >> /home/vagrant/.bashrc
+echo ' _____  _____ _               _   _ _____   ____  _____            '
+echo '|_   _|/ ____| |        /\   | \ | |  __ \ / __ \|  __ \     /\    '
+echo '  | | | (___ | |       /  \  |  \| | |  | | |  | | |__) |   /  \   '
+echo '  | |  \___ \| |      / /\ \ | . ` | |  | | |  | |  _  /   / /\ \  '
+echo ' _| |_ ____) | |____ / ____ \| |\  | |__| | |__| | | \ \  / ____ \ '
+echo '|_____|_____/|______/_/    \_\_| \_|_____/ \____/|_|  \_\/_/    \_\'
+echo '                                (                '
+echo '     )                )     )   )\ )   (      )  '
+echo '  ( /(     )       ( /(  ( /(  (()/(   )\  ( /(  '
+echo '  )\()) ( /(  ___  )\()) )\())  /(_))(((_) )\()) '
+echo ' ((_)\  )\())|___|((_)\ ((_)\  (_))  )\___((_)\  '
+echo '|__  / ((_)\       / (_)/ _(_) | _ \((/ __|/ (_) '
+echo '  / /_ \ \ /       | | _\_, /  |   / | (__ | |   '
+echo ' /_/(_)/_\_\       |_|(_)/_/   |_|_\  \___||_|   '
+echo '                                                 '
+EOT
